e6b6e94f2a5ce748cf0f9d3b29e2a05c60e42c32
adelNfahmy10 <fahmyan@taibaconnect.com>
2025-02-17 15:19:00 +0200

    about section is finish

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withInMemoryScrolling({scrollPositionRestoration:'top'})), provideClientHydration()]
+  providers: [provideRouter(routes, withInMemoryScrolling({scrollPositionRestoration:'top'})),
+    provideClientHydration(),
+  ]
 };
